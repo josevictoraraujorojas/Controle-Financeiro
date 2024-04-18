a46f017368b4bfe6298369824d2ea00e0233f974
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-04-18 18:06:46 -0300

    Grafico de barra marcacao

--- Projeto/src/Visao/GuiPrincipal.java
+++ Projeto/src/Visao/GuiPrincipal.java
@@ -6,6 +6,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.annotations.CategoryTextAnnotation;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.ValueMarker;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
@@ -51,23 +52,27 @@ public class GuiPrincipal extends JFrame {
         barra.addValue( 400, "despesas variaveis","despesas variaveis" );
         graficoBarra = ChartFactory.createBarChart("Orcamento","tipos de gastos","quantidade em real",barra,PlotOrientation.HORIZONTAL,true,true,false);
 
-        CategoryPlot teste = (CategoryPlot) graficoBarra.getPlot();
-        CategoryTextAnnotation anotacao = new CategoryTextAnnotation("Limite das metas:"+0.2*1400,"metas",90);
-        anotacao.setPaint(Color.RED);
-        anotacao.setFont(fonte);
-        teste.addAnnotation(anotacao);
+        CategoryPlot plot = (CategoryPlot) graficoBarra.getPlot();
 
-        CategoryPlot teste1 = (CategoryPlot) graficoBarra.getPlot();
-        CategoryTextAnnotation anotacao1 = new CategoryTextAnnotation("Limite despesas fixas:"+0.5*1400,"despesas fixas",105);
-        anotacao1.setPaint(Color.RED);
-        anotacao1.setFont(fonte);
-        teste.addAnnotation(anotacao1);
+        plot.getRangeAxis().setRange(0, 1400); // Altere 800 para o valor máximo desejado
 
-        CategoryPlot teste2 = (CategoryPlot) graficoBarra.getPlot();
-        CategoryTextAnnotation anotacao2 = new CategoryTextAnnotation("Limite despesas variaveis:"+0.3*1400,"despesas variaveis",120);
-        anotacao2.setPaint(Color.RED);
-        anotacao2.setFont(fonte);
-        teste.addAnnotation(anotacao2);
+        double limiteMetas = 0.2 * 1400; // Limite para metas
+        ValueMarker markerMetas = new ValueMarker(limiteMetas);
+        markerMetas.setPaint(Color.RED);
+        markerMetas.setStroke(new BasicStroke(3f)); // Defina a largura desejada
+        plot.addRangeMarker(markerMetas);
+
+        double limiteDespesasFixas = 0.5 * 1400; // Limite para despesas fixas
+        ValueMarker markerDespesasFixas = new ValueMarker(limiteDespesasFixas);
+        markerDespesasFixas.setPaint(Color.blue);
+        markerDespesasFixas.setStroke(new BasicStroke(3f)); // Defina a largura desejada
+        plot.addRangeMarker(markerDespesasFixas);
+
+        double limiteDespesasVariaveis = 0.3 * 1400; // Limite para despesas variaveis
+        ValueMarker markerDespesasVariaveis = new ValueMarker(limiteDespesasVariaveis);
+        markerDespesasVariaveis.setPaint(Color.green);
+        markerDespesasVariaveis.setStroke(new BasicStroke(3f)); // Defina a largura desejada
+        plot.addRangeMarker(markerDespesasVariaveis);
 
         painelGraficoBarra = new ChartPanel(graficoBarra);
         fundo.add(painelGraficoBarra,painelGraficoBarraC);
