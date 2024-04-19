cb7ca7202cb2b3ce60c0f4bceed34f96b8136f1e
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-04-19 15:42:19 -0300

    lista  despesas

--- Projeto/src/Visao/GuiPrincipal.java
+++ Projeto/src/Visao/GuiPrincipal.java
@@ -17,41 +17,69 @@ public class GuiPrincipal extends JFrame {
 
     JPanelComImagemDeFundo fundo;
     GridBagConstraints fundoC;
+    JPanelComImagemDeFundo painelGraficos;
+    GridBagConstraints painelGraficosC;
+    JPanelComImagemDeFundo painelLista;
+    GridBagConstraints painelListaC;
     JFreeChart graficoPizza;
     ChartPanel painelGraficoPizza;
     GridBagConstraints painelGraficoPizzaC;
     JFreeChart graficoBarra;
     ChartPanel painelGraficoBarra;
     GridBagConstraints painelGraficoBarraC;
+    JList lista;
+    GridBagConstraints listaC;
+    private static  final String[] despesasFixas = {"conta de agua","conta de luz","conta supermercado"};
     public GuiPrincipal() {
         super("grafico");
         setLayout(new GridBagLayout());
         Font fonte = new Font("Serif",Font.PLAIN,16);
-
-        painelGraficoPizzaC = new GridBagConstraints();
-        GridBagConstraints barraC = new GridBagConstraints();
 
         fundoC = new GridBagConstraints();
         fundoC.weighty = 1;
         fundoC.weightx = 1;
         fundoC.fill = GridBagConstraints.BOTH;
 
+        painelGraficosC = new GridBagConstraints();
+        painelGraficosC.gridwidth=GridBagConstraints.REMAINDER;
+
+        painelListaC = new GridBagConstraints();
+
+        painelGraficoPizzaC = new GridBagConstraints();
+        painelGraficoPizzaC.ipady=-200;
+        painelGraficoPizzaC.ipadx=-200;
+
+        painelGraficoBarraC = new GridBagConstraints();
+        painelGraficoBarraC.ipady=-200;
+        painelGraficoBarraC.ipadx=-200;
+
+        listaC = new GridBagConstraints();
+        listaC.ipadx=839;
+        listaC.ipady=600;
+
+
+
         fundo = new JPanelComImagemDeFundo(getClass().getResource("Imagem\\contas-a-pagar-e-receber.jpeg"));
         fundo.setLayout(new GridBagLayout());
         add(fundo,fundoC);
+
+        painelGraficos = new JPanelComImagemDeFundo(getClass().getResource("Imagem\\fundo-quadrado-azul-para-cartaz-de-banner-anuncio-de-midia-social-evento-e-varios-trabalhos-de-design_7954-52368.jpg"));
+        painelGraficos.setLayout(new GridBagLayout());
+        fundo.add(painelGraficos,painelGraficosC);
 
         DefaultPieDataset pizza = new DefaultPieDataset();
         pizza.setValue("metas",0.2*1400);
         pizza.setValue("despesas fixas",0.5*1400);
         pizza.setValue("despesas variaveis",0.3*1400);
         graficoPizza = ChartFactory.createPieChart("limite do orcamento",pizza,true,true,false);
+        painelGraficoPizza = new ChartPanel(graficoPizza);
+        painelGraficos.add(painelGraficoPizza,painelGraficoPizzaC);
 
         DefaultCategoryDataset barra = new DefaultCategoryDataset();
         barra.addValue( 200, "Metas","metas");
         barra.addValue( 600, "despesas fixas","despesas fixas" );
         barra.addValue( 400, "despesas variaveis","despesas variaveis" );
         graficoBarra = ChartFactory.createBarChart("Orcamento","tipos de gastos","quantidade em real",barra,PlotOrientation.HORIZONTAL,true,true,false);
-
         CategoryPlot plot = (CategoryPlot) graficoBarra.getPlot();
 
         plot.getRangeAxis().setRange(0, 1400); // Altere 800 para o valor máximo desejado
@@ -75,12 +103,16 @@ public class GuiPrincipal extends JFrame {
         plot.addRangeMarker(markerDespesasVariaveis);
 
         painelGraficoBarra = new ChartPanel(graficoBarra);
-        fundo.add(painelGraficoBarra,painelGraficoBarraC);
+        painelGraficos.add(painelGraficoBarra,painelGraficoBarraC);
 
-        painelGraficoPizza = new ChartPanel(graficoPizza);
-        fundo.add(painelGraficoPizza,painelGraficoPizzaC);
+        painelLista = new JPanelComImagemDeFundo(getClass().getResource("Imagem\\fundo-quadrado-azul-para-cartaz-de-banner-anuncio-de-midia-social-evento-e-varios-trabalhos-de-design_7954-52368.jpg"));
+        painelLista.setLayout(new GridBagLayout());
+        fundo.add(painelLista,painelListaC);
 
-
+        lista = new JList<>(despesasFixas);
+        lista.setVisibleRowCount(5);
+        lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        painelLista.add(lista,listaC);
 
 
     }
